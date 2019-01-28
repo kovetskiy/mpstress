b4183a4aca395d200021f4f42efebe51303dc86f
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-01-28 18:09:08 +0300

    also print index.html

--- plot.bash
+++ plot.bash
@@ -10,7 +10,7 @@ set format x \"%H:%M:%S\"
 set ytics 10
 set grid
 set style data lines
-set term png size 1200, 500
+set term png size 2000, 500
 set yrange [0:100]
 "
 
@@ -23,6 +23,7 @@ ${common}
 set output "output/$host.png"
 plot \
     "output/$host/total" using 1:2 title "total" with lines, \\
+    "output/$host/iowait" using 1:2 title "iowait" with lines, \\
     "output/$host/usr" using 1:2 title "usr" with lines, \\
     "output/$host/sys" using 1:2 title "sys" with lines
 HEAD
@@ -39,3 +40,12 @@ for host in $(find ./output/ -maxdepth 1 -type d -printf '%P\n' | sort -n); do
 done
 
 gnuplot <<< "$summary"
+
+rm -f output/index.html
+find ./output/ -maxdepth 1 -type f -name '*.png' -printf '%P\n' \
+    | sort -n \
+    | while read img; do
+    cat >> output/index.html <<HTML
+    <img src="$img" /><br/><br />
+HTML
+done
